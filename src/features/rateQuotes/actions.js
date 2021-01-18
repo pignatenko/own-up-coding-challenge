5567690067e0f1320ccffe438988522f98fb41f3
Paul Ignatenko <pignatenko@ambulnz.com>
2021-01-18 14:27:14 -0500

    Rule of 3 refactor actions to be more generic so that tests are green.

--- src/features/rateQuotes/actions.js
+++ src/features/rateQuotes/actions.js
@@ -1,22 +1,24 @@
 const FEATURE = 'rateQuotes';
 
-export function setLoanSize(loanSize){
-  return {
-    type: `${FEATURE}/setLoanSize`,
+function createSetterAction(name, payloadPropertyName, payload) {
+  const action = {
+    type: `${FEATURE}/${name}`,
     payload: {
-      loanSize
     }
   };
+
+  action.payload[payloadPropertyName] = payload;
+  return action;
+}
+
+export function setLoanSize(loanSize){
+  return createSetterAction('setLoanSize', 'loanSize', loanSize);
 }
 
 export function setCreditScore(creditScore){
-  return {
-    type: `${FEATURE}/setCreditScore`,
-    payload: {
-      creditScore
-    }
-  };
+  return createSetterAction('setCreditScore', 'creditScore', creditScore);
 }
 
 export function setPropertyType(propertyType){
+  return createSetterAction('setPropertyType', 'propertyType', propertyType);
 }
