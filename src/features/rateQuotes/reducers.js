be0f9ace433db67920d68a33677ec7ad47d828c2
Paul Ignatenko <pignatenko@ambulnz.com>
2021-01-18 17:45:14 -0500

    Create reducer table tests along with reducers

---
+++ src/features/rateQuotes/reducers.js
@@ -0,0 +1,87 @@
+import { PROPERTY_TYPE, OCCUPANCY } from '../../enums';
+import { FEATURE_NAME } from '.';
+
+
+function reduceActionPayload(eventName, state, action) {
+  if (action.type !== eventName) {
+    return state;
+  }
+
+  return {
+    ...state,
+    ...action.payload
+  };
+};
+
+
+
+export const initialSetLoanSizeState = {
+  loanSize: 0,
+}
+
+export function reduceSetLoanSize(state = initialSetLoanSizeState, action) {
+  return reduceActionPayload(`${FEATURE_NAME}/setLoanSize`, state, action);
+}
+
+export const initialSetCreditScoreState = {
+  creditScore: 600
+}
+
+export function reduceSetCreditScore(state = initialSetCreditScoreState, action){
+  return reduceActionPayload(`${FEATURE_NAME}/setCreditScore`, state, action);
+}
+
+export const initialSetPropertyTypeState = {
+  propertyType: PROPERTY_TYPE.SingleFamily
+}
+
+export function reduceSetPropertyType(state = initialSetPropertyTypeState, action){
+  return reduceActionPayload(`${FEATURE_NAME}/setPropertyType`, state, action);
+}
+
+export const initialSetOccupancyState = {
+  occupancy: OCCUPANCY.Primary,
+};
+
+export function reduceSetOccupancy(state = initialSetOccupancyState, action){
+  return reduceActionPayload(`${FEATURE_NAME}/setOccupancy`, state, action);
+}
+
+export const initialSetRateQuotesState = {
+  rateQuotes: [],
+};
+
+export function reduceSetRateQuotes(state = initialSetRateQuotesState, action){
+  return reduceActionPayload(`${FEATURE_NAME}/setRateQuotes`, state, action);
+}
+
+export const initialSetRateQuoteErrorsState = {
+  rateQuoteErrors: [],
+};
+
+export function reduceSetRateQuoteErrors(state = initialSetRateQuoteErrorsState, action){
+  return reduceActionPayload(`${FEATURE_NAME}/setRateQuoteErrors`, state, action);
+}
+
+export const initialRateQuotesState = Object.assign(
+  {},
+  initialSetLoanSizeState,
+  initialSetCreditScoreState,
+  initialSetPropertyTypeState,
+  initialSetOccupancyState,
+  initialSetRateQuotesState,
+  initialSetRateQuoteErrorsState,
+);
+
+const reducers = [
+  reduceSetLoanSize,
+  reduceSetCreditScore,
+  reduceSetPropertyType,
+  reduceSetOccupancy,
+  reduceSetRateQuotes,
+  reduceSetRateQuoteErrors,
+];
+
+export function reduceRateQuotes(state = initialRateQuotesState, action) {
+  return reducers.reduce((newState, reducer ) => reducer(newState, action), state);
+};
---
+++ src/features/rateQuotes/reducers.test.js
@@ -0,0 +1,138 @@
+import {OCCUPANCY, PROPERTY_TYPE} from '../../enums';
+import { RateQuoteItemFactory } from "../../factories/api/getRatesResponseFactory";
+
+import {
+  reduceSetLoanSize,
+  reduceSetCreditScore,
+  reduceSetPropertyType,
+  reduceSetOccupancy,
+  reduceSetRateQuotes,
+  reduceSetRateQuoteErrors,
+  reduceRateQuotes,
+  initialSetLoanSizeState,
+  initialSetCreditScoreState,
+  initialSetPropertyTypeState,
+  initialSetOccupancyState,
+  initialSetRateQuotesState,
+  initialSetRateQuoteErrorsState,
+  initialRateQuotesState,
+} from "./reducers";
+
+describe('RateQuote Reducers', () => {
+  let action;
+  let state;
+  const wrongAction = {
+    type: 'noSuchFeature/noSuchEvent',
+    payload: {
+      nothing: 123,
+    }
+  };
+  const tests = [
+    {
+      function: reduceSetLoanSize,
+      initial: initialSetLoanSizeState,
+      type: 'rateQuotes/setLoanSize',
+      payload: {
+        loanSize: 100000,
+      },
+    },
+    {
+      function: reduceSetCreditScore,
+      initial: initialSetCreditScoreState,
+      type: 'rateQuotes/setCreditScore',
+      payload: {
+        creditScore: 800,
+      },
+    },
+    {
+      function: reduceSetOccupancy,
+      initial: initialSetOccupancyState,
+      type: 'rateQuotes/setOccupancy',
+      payload: {
+        occupancy: OCCUPANCY.Secondary,
+      },
+    },
+    {
+      function: reduceSetPropertyType,
+      initial: initialSetPropertyTypeState,
+      type: 'rateQuotes/setPropertyType',
+      payload: {
+        propertyType: PROPERTY_TYPE.Condo,
+      },
+    },
+    {
+      function: reduceSetRateQuotes,
+      initial: initialSetRateQuotesState,
+      type: 'rateQuotes/setRateQuotes',
+      payload: {
+        rateQuotes: [ RateQuoteItemFactory() ],
+      },
+    },
+    {
+      function: reduceSetRateQuoteErrors,
+      initial: initialSetRateQuoteErrorsState,
+      type: 'rateQuotes/setRateQuoteErrors',
+      payload: {
+        rateQuoteErrors: ['Something went wrong'],
+      },
+    },
+  ]
+
+
+  tests.forEach(test => {
+    describe(`#${test.function.name}`, () => {
+      beforeEach(() => {
+        state = {}
+        action = {
+          type: test.type,
+          payload: test.payload,
+        };
+      });
+
+      it(`triggers on action.type = ${test.type}`, () => {
+        const newState = test.function(state, action);
+        expect(newState).not.toBe(state);
+        expect(newState).toStrictEqual(action.payload);
+      });
+
+      it(`does not trigger when sent any other type`, () => {
+        const newState = test.function(state, wrongAction);
+        expect(newState).toBe(state);
+      });
+
+      it(`sets the default state if undefined`, () => {
+        const newState = test.function(undefined, wrongAction);
+        expect(newState).toBe(test.initial);
+      });
+    });
+  });
+
+  describe(`#reduceRateQuotes`, () => {
+
+    it(`does not trigger on unhandled actions`, () => {
+      const newState = reduceRateQuotes(initialRateQuotesState, wrongAction);
+      expect(newState).toBe(initialRateQuotesState);
+    });
+
+    it(`sets the default state if undefined`, () => {
+      const newState = reduceRateQuotes(undefined, wrongAction);
+      expect(newState).toBe(initialRateQuotesState);
+    });
+
+    tests.forEach(test => {
+      beforeEach(() => {
+        state = {}
+        action = {
+          type: test.type,
+          payload: test.payload,
+        };
+      });
+
+      it(`triggers on action.type = ${test.type}`, () => {
+        const newState = reduceRateQuotes(state, action);
+        expect(newState).not.toBe(state);
+        expect(newState).toStrictEqual(action.payload);
+      });
+    });
+  });
+});
